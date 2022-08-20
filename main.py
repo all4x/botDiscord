b0dba39bbf54affece9d42abc8e38236ac542603
all4x <estudar0101@gmail.com>
2022-08-20 11:16:37 -0300

    first version well done!

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
- 
+import sys
 import requests
 from bs4 import BeautifulSoup
 
+def cfg_csgo():
 
-def cfg_config_csgo():
-
-    state_url = ('https://prosettings.net/players/s1mple')
+    state_url = (f'https://prosettings.net/players/{sys.argv[1]}')
     page = requests.get(state_url)
-    print(page.status_code)
+    status = page.status_code
+    
+    if status <= 200:    
+       
+        soup = BeautifulSoup(page.content, 'html.parser')
+        indicadors = soup.select('.content')[0].text 
+        cfg = soup.select('.download')[0].get('href')
+        print(indicadors, cfg)
+    
+    else:
+   
+        print('O nome do jogador está errado, burro pra carai...')
 
-    soup = BeautifulSoup(page.content, 'html.parser')
-    indicadors = soup.select('.center-container') 
-
-
-    print(indicadors)
-
-    cfg_config_csgo()
+cfg_csgo()
